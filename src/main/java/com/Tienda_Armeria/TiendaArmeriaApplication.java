ba58879afc54d5ea53280bb70e2fdcb5353d1f3f
Nazih <Nazih@DESKTOP-N382D65>
2022-06-05 12:47:04 -0600

    Tarea #2 de Aplicaciones Web

---
+++ src/main/java/com/Tienda_Armeria/TiendaArmeriaApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_Armeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaArmeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaArmeriaApplication.class, args);
+	}
+
+}
